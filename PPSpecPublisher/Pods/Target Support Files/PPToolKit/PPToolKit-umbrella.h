035c722a7cc2d1f4e73d1f9d8905604e9567c19b
Garenge <garenge@outlook.com>
2025-07-13 16:46:25 +0800

    pod add PPCatalystTool, PPToolKit

---
+++ PPSpecPublisher/Pods/Target Support Files/PPToolKit/PPToolKit-umbrella.h
@@ -0,0 +1,32 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSArray+ppEx.h"
+#import "NSDictionary+ppEx.h"
+#import "NSString+ppEx.h"
+#import "UIButton+ppEx.h"
+#import "UIView+ppEx.h"
+#import "Config.h"
+#import "PPScrollContentView.h"
+#import "PPToolKit.h"
+#import "NSArray+ppEx.h"
+#import "NSDictionary+ppEx.h"
+#import "NSString+ppEx.h"
+#import "UIButton+ppEx.h"
+#import "UIView+ppEx.h"
+#import "Config.h"
+#import "PPScrollContentView.h"
+#import "PPToolKit.h"
+
+FOUNDATION_EXPORT double PPToolKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char PPToolKitVersionString[];
+
